b96a2b73ba26e82ab9caec3696471d01d06dd3ae
Fujii Hironori <fujii.hironori@gmail.com>
2015-02-02 22:20:36 +0900

    Revert "emacs.el: Add Wanderlust package archive."
    This reverts commit 9caa1ba169cd7e781822bfb9bcee0948bc7f95a1.

--- emacs.el
+++ emacs.el
@@ -105,9 +105,6 @@
 (require 'package)
 (add-to-list 'package-archives
   '("melpa" . "http://melpa.milkbox.net/packages/") t)
-; Wanderlust
-(add-to-list 'package-archives
-  '("e6h" . "http://www.e6h.org/packages/"))
 
 ;; misc function
 
