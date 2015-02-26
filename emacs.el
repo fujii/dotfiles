5aa33e12331e232c9a606b6dff13286f84b66834
Fujii Hironori <fujii.hironori@gmail.com>
2015-02-27 00:02:01 +0900

    emacs.el: Do not call unnecessary set-default-coding-systems, set-selection-coding-system and set-terminal-coding-system .

--- emacs.el
+++ emacs.el
@@ -7,10 +7,6 @@
 ;; coding-system
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-;(set-selection-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
 
 ;; key bind
 (define-key global-map "\C-o" 'toggle-input-method)
