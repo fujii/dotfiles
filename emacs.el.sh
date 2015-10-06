b296f142b1a3cb92c51a159b95d0ebbe398248c3
Fujii Hironori <fujii.hironori@gmail.com>
2015-10-06 22:59:16 +0900

    user-full-name was not set.
    emacs.el.sh: Fix typo.

--- emacs.el.sh
+++ emacs.el.sh
@@ -1,4 +1,4 @@
 a="s/(setq user-mail-address \".*\")/(setq user-mail-address \"${user_email}\")/"
-b="s/(setq user-full-name \".*\")/(setq user-full-address \"${user_name}\")/"
+b="s/(setq user-full-name \".*\")/(setq user-full-name \"${user_name}\")/"
 
 sed -e "$a;$b" emacs.el
